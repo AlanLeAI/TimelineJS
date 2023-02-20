63b7b6f24d8a7ea4eac0e3527a1bc070841daa3f
Tuan Anh Le <58692117+AlanLeAI@users.noreply.github.com>
2023-02-19 20:44:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,146 @@
-(function($) {
-  $.fn.timeline = function() {
-    var selectors = {
-      id: $(this),
-      item: $(this).find(".timeline-item"),
-      activeClass: "timeline-item--active",
-      img: ".timeline__img"
-    };
-    selectors.item.eq(0).addClass(selectors.activeClass);
-    selectors.id.css(
-      "background-image",
-      "url(" +
-        selectors.item
-          .first()
-          .find(selectors.img)
-          .attr("src") +
-        ")"
-    );
-    var itemLength = selectors.item.length;
-    $(window).scroll(function() {
-      var max, min;
-      var pos = $(this).scrollTop();
-      selectors.item.each(function(i) {
-        min = $(this).offset().top;
-        max = $(this).height() + $(this).offset().top;
-        var that = $(this);
-        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
-          selectors.item.removeClass(selectors.activeClass);
-          selectors.id.css(
-            "background-image",
-            "url(" +
-              selectors.item
-                .last()
-                .find(selectors.img)
-                .attr("src") +
-              ")"
-          );
-          selectors.item.last().addClass(selectors.activeClass);
-        } else if (pos <= max - 40 && pos >= min) {
-          selectors.id.css(
-            "background-image",
-            "url(" +
-              $(this)
-                .find(selectors.img)
-                .attr("src") +
-              ")"
-          );
-          selectors.item.removeClass(selectors.activeClass);
-          $(this).addClass(selectors.activeClass);
-        }
-      });
-    });
-  };
-})(jQuery);
+(function () {
+	"use strict";
 
-$("#timeline-1").timeline();
+	let ill = {};
+
+	// viewports etc
+
+	ill.elementIsVisible = function(el) {
+		let rect = el.getBoundingClientRect(),
+			viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+		return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+	};
+
+	ill.ensureElementInView = function(el) {
+		if (!ill.elementIsVisible(el)) {
+			el.scrollIntoView({behavior: "smooth"});
+		}
+	};
+
+	// events
+
+	ill.unselectAllRecords = function() {
+		[].forEach.call(document.querySelectorAll(".illustrated .record.selected, .illustrated .calculation.selected"),
+		function(el) {
+			el.classList.remove("selected");
+		});
+	};
+
+	ill.toggleRecord = function(element, event) {
+		let selected = element.classList.contains("selected");
+		ill.unselectAllRecords();
+		if (!selected) {
+			element.classList.add("selected");
+		} else {
+			ill.closeAllCode();
+		}
+		ill.cancel(event);
+		ill.ensureElementInView(element);
+	};
+
+	ill.selectRecord = function(element, event) {
+		ill.unselectAllRecords();
+		element.classList.add("selected");
+		ill.cancel(event);
+		ill.ensureElementInView(element);
+	};
+
+	ill.showCode = function(element, event) {
+		element.parentNode.classList.add("show");
+		ill.cancel(event);
+	};
+
+	ill.closeAllCode = function() {
+		[].forEach.call(document.querySelectorAll("codesample.show"), function(el) {
+			el.classList.remove("show");
+		});
+	};
+
+	ill.toggleAnnotate = function(el) {
+		el.classList.toggle("annotate");
+	};
+
+	ill.cancel = function(event) {
+		if (event) { event.stopPropagation(); }
+	};
+
+	// injections
+
+	ill.addShowCode = function(el) {
+		el.innerHTML = document.getElementById("showCodeTmpl").innerHTML + el.innerHTML;
+	};
+
+	ill.addToggleAnnotations = function(record) {
+		let expl = record.querySelector(".rec-explanation"),
+			copy = document.getElementById("annotateTmpl").cloneNode(true);
+		expl.insertAdjacentElement("afterend", copy);
+	};
+
+	ill.injectLabels = function() {
+		let els = document.querySelectorAll(".string > .explanation, .decryption > .explanation");
+		[].forEach.call(els, function(expl) {
+			let label = expl.parentNode.querySelector(".label"),
+				h4 = document.createElement("h4");
+			h4.appendChild(document.createTextNode(label.textContent));
+			expl.insertAdjacentElement("afterbegin", h4);
+		});
+	};
+
+	ill.printMode = function() {
+		// add printmode css
+		let inject = document.createElement("link");
+		inject.setAttribute("rel", "stylesheet");
+		inject.setAttribute("href", "printmode.css");
+		document.head.appendChild(inject);
+		// open everything up
+		[].forEach.call(document.querySelectorAll(".record, .calculation"), function(el){
+			el.classList.add("selected");
+			el.classList.add("annotate");
+		});
+		[].forEach.call(document.querySelectorAll("codesample"), function(el){
+			el.classList.add("show");
+		});
+		[].forEach.call(document.querySelectorAll("*"), function(el) {
+			el.onclick = null;
+		});
+	};
+
+
+	window.onload = function() {
+		[].forEach.call(document.querySelectorAll(".record, .calculation"), function(el) {
+			el.onclick = function(event) {
+				if (el === event.target && event.offsetY < 60) {
+					ill.toggleRecord(el, event);
+				} else {
+					ill.selectRecord(el, event);
+				}
+			};
+		});
+		[].forEach.call(document.querySelectorAll(".rec-label"), function(el) {
+			el.onclick = function(event) {
+				ill.toggleRecord(el.parentNode, event);
+			};
+		});
+		[].forEach.call(document.querySelectorAll(".record"), function(el) {
+			ill.addToggleAnnotations(el);
+		});
+		[].forEach.call(document.querySelectorAll("codesample"), function(el) {
+			ill.addShowCode(el);
+		});
+		ill.injectLabels();
+	};
+
+	window.onkeyup = function(e) {
+		let els;
+		if (e.keyCode === 27) {
+			els = document.querySelectorAll(".record.annotate");
+			if (els.length) {
+				[].forEach.call(els, function(rec) { rec.classList.remove("annotate"); });
+			} else {
+				ill.unselectAllRecords();
+			}
+		}
+	};
+
+	window.ill = ill;
+})();
